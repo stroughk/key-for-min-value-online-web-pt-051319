790203ae64a26caee679e4b6525bd4c731df0e2c
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-26 18:44:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
+name_hash = {:chair => 25, :table => 85, :mattress => 450}  
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ name_hash.collect do |item, price|
+   item 
+ end
 end
